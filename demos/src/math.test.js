f68db6982f94c70710c206ac878a0d61fd10de7e
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-10-08 20:59:55 -0300

    Pruebas estáticas con ESLint y Prettier

--- demos/src/math.test.js
+++ demos/src/math.test.js
@@ -1,21 +1,21 @@
-const { sum, multiply, divide } = require('./math')
+const { sum, multiply, divide } = require('./math');
 
 test('add 1 + 4 should be 5', () => {
-	const rta = sum(1, 4)
-	expect(rta).toBe(5)
-})
+  const rta = sum(1, 4);
+  expect(rta).toBe(5);
+});
 
 test('5*5 should be 25', () => {
-	const rta = multiply(5, 5)
-	expect(rta).toBe(25)
-})
+  const rta = multiply(5, 5);
+  expect(rta).toBe(25);
+});
 
 test('6 / 3 should be 2', () => {
-	const rta = divide(6, 3)
-	expect(rta).toBe(2)
-})
+  const rta = divide(6, 3);
+  expect(rta).toBe(2);
+});
 
 test('divide for zero should be null', () => {
-	const rta = divide(5, 0)
-	expect(rta).toBe(null)
-})
+  const rta = divide(5, 0);
+  expect(rta).toBe(null);
+});
